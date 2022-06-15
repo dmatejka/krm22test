6a8af7be4011cbbd5c69243ecbf92b522000720e
Daniel Matejka <matejka@xpression.sk>
2022-06-15 17:47:28 +0200

    shell and pwa

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'krm-app-shell',
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.scss']
+})
+export class AppShellComponent {
+
+}
